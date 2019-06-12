bd560bab7949d00026f7831fe132f20f8e54b285
Pavel Růžička <hello@pavelruzicka.com>
2019-06-12 16:21:55 +0200

    feat: field creation

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,20 @@
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+
+  data() {
+    return {};
+  },
+
+  created() {
+    this.fields = Array(40).fill({ occupied: 0, special: null });
+
+    this.fields[0] = { occupied: 0, special: "green" };
+    this.fields[10] = { occupied: 0, special: "red" };
+    this.fields[20] = { occupied: 0, special: "blue" };
+    this.fields[30] = { occupied: 0, special: "yellow" };
+  }
 };
 </script>
 
