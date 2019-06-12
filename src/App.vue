a67a69f7562499f2911a58871a708f2d419d62f6
Pavel Růžička <hello@pavelruzicka.com>
2019-06-12 21:49:15 +0200

    feat: visualize grid

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,126 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="board">
+      <div class="row row--horizontal">
+        <div class="row row--vertical row--end row--reversed">
+          <div class="row row--horizontal">
+            <Field
+              v-for="field in fields.slice(0, 5)"
+              :special="field.special"
+              :key="field.index"
+            >{{ field.index }}</Field>
+          </div>
+          <Field
+            v-for="field in fields.slice(5, 9)"
+            :special="field.special"
+            :key="field.index"
+          >{{ field.index }}</Field>
+        </div>
+
+        <div class="row row--vertical">
+          <Field
+            v-for="field in fields.slice(9, 10)"
+            :special="field.special"
+            :key="field.index"
+          >{{ field.index }}</Field>
+          <Field
+            v-for="(field, index) in [null, null, null, null]"
+            special="red"
+            :key="index + 100"
+          ></Field>
+        </div>
+
+        <div class="row row--vertical">
+          <Field
+            v-for="field in fields.slice(10, 14)"
+            :special="field.special"
+            :key="field.index"
+          >{{ field.index }}</Field>
+          <div class="row row--horizontal">
+            <Field
+              v-for="field in fields.slice(14, 19)"
+              :special="field.special"
+              :key="field.index"
+            >{{ field.index }}</Field>
+          </div>
+        </div>
+      </div>
+
+      <div class="row row--horizontal">
+        <Field
+          v-for="field in fields.slice(39, 40)"
+          :special="field.special"
+          :key="field.index"
+        >{{ field.index }}</Field>
+        <Field
+          v-for="(field, index) in [null, null, null, null]"
+          special="green"
+          :key="index + 200"
+        ></Field>
+        <Field empty="true"/>
+        <Field v-for="(field, index) in [null, null, null, null]" special="blue" :key="index + 300"></Field>
+        <Field
+          v-for="field in fields.slice(19, 20)"
+          :special="field.special"
+          :key="field.index"
+        >{{ field.index }}</Field>
+      </div>
+
+      <div class="row row--horizontal">
+        <div class="row row--vertical row--end">
+          <div class="row row--horizontal">
+            <Field
+              v-for="field in fields.slice(34, 39).reverse()"
+              :special="field.special"
+              :key="field.index"
+            >{{ field.index }}</Field>
+          </div>
+          <Field
+            v-for="field in fields.slice(30, 34).reverse()"
+            :special="field.special"
+            :key="field.index"
+          >{{ field.index }}</Field>
+        </div>
+
+        <div class="row row--vertical">
+          <Field
+            v-for="(field, index) in [null, null, null, null]"
+            special="yellow"
+            :key="index + 400"
+          ></Field>
+          <Field
+            v-for="field in fields.slice(29, 30)"
+            :special="field.special"
+            :key="field.index"
+          >{{ field.index }}</Field>
+        </div>
+
+        <div class="row row--vertical">
+          <div class="row row--horizontal">
+            <Field
+              v-for="field in fields.slice(20, 25).reverse()"
+              :special="field.special"
+              :key="field.index"
+            >{{ field.index }}</Field>
+          </div>
+          <Field
+            v-for="field in fields.slice(25, 29)"
+            :special="field.special"
+            :key="field.index"
+          >{{ field.index }}</Field>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Field from "./components/Field.vue";
+
 export default {
   name: "app",
+
+  components: { Field },
 
   data() {
     return {};
@@ -17,10 +133,35 @@ export default {
     this.fields[10] = { occupied: 0, special: "red" };
     this.fields[20] = { occupied: 0, special: "blue" };
     this.fields[30] = { occupied: 0, special: "yellow" };
+
+    this.fields = this.fields.map((field, index) => ({ ...field, index }));
   }
 };
 </script>
 
 <style lang="sass">
+#app
+  margin: 3rem 0
+  display: flex
+  justify-content: center
+  align-items: center
 
+.row
+  display: flex
+  flex: 0 0 auto
+  
+  &--horizontal
+    flex-direction: row
+
+    &.row--reversed
+      flex-direction: row-reverse
+
+  &--vertical
+    flex-direction: column
+
+    &.row--reversed
+      flex-direction: column-reverse
+
+  &--end *
+    align-self: flex-end
 </style>
---
+++ src/components/Field.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="field" :class="[special ? `field--${special}` : null, empty ? `field--empty` : null]">
+    <!-- <slot></slot> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "field",
+  props: ["empty", "special"]
+};
+</script>
+
+<style lang="sass">
+.field
+  width: 2.5rem
+  height: 2.5rem
+  border: .2rem solid black
+  flex: 0 0 2.5rem
+  display: flex
+  justify-content: center
+  align-items: center
+  border-radius: 50%
+  margin: .35rem
+
+  &--empty
+    border-color: transparent
+
+  &--green
+    background: rgb(52, 199, 89)
+
+  &--red
+    background: rgb(255, 59, 48)
+
+  &--blue
+    background: rgb(0, 122, 255)
+
+  &--yellow
+    background: rgb(255, 204, 0)
+</style>
