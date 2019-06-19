ed331c9a5e8bb3d9bb8d06f6b1a2d5d16beed987
Pavel Růžička <hello@pavelruzicka.com>
2019-06-20 01:09:33 +0200

    docs: logic methods documentation

--- src/logic/matchingPiece.js
+++ src/logic/matchingPiece.js
@@ -1,2 +1,10 @@
+/**
+ * Selects matching piece from piece bank
+ *
+ * @param {Object} state - Central app state
+ * @param {String} color - Color of sought-after game piece
+ * @param {String} piece - Notation of sought-after game piece
+ * @return {Object} Matching piece
+ */
 export default (state, color, piece) =>
   state.pieces.filter(p => p.color === color && p.piece === piece)[0];
--- src/logic/targetField.js
+++ src/logic/targetField.js
@@ -1,5 +1,13 @@
 import constants from "../constants";
 
+/**
+ * Determines index of target field
+ *
+ * @param {Number} position - Default field index
+ * @param {Number} increment - Field amount of desired advancement
+ * @param {String} color - Player color
+ * @return {Number} Target field index
+ */
 export default (position, increment, color) => {
   const range = constants.positions.precedingHome[color];
 
